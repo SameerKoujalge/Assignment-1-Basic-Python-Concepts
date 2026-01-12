e4e6ffc787ecb06e419f711ccbb488c5c56aaf84
Sameer Koujalage <159868207+SameerKoujalge@users.noreply.github.com>
2026-01-12 17:45:14 +0530

    Add files via upload

---
+++ Assignment 1 Create a personalized greeting.py
@@ -0,0 +1,8 @@
+# Task 2 - Create a personalized greeting
+
+first_name=input("Enter your first name:")
+last_name=input("Enter your lasr_name:")
+
+full_name=first_name+" "+last_name
+
+print(f" Hello,{full_name}! Welcome to the python program.")
